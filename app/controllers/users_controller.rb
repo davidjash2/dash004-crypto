f8bf89a5922db0acee3f5a51ea308b28c1dd9523
David Ash <davidjash2@gmail.com>
2024-11-29 07:56:42 -0500

    fixed broken stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      #@user = User.find(params.expect(:id))
+      # @user = User.find(params.expect(:id))
       @user = User.find(1)
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 include BCrypt
 
 class User < ApplicationRecord
-
   def save
     self.password = Password.create(self.password)
     super()
@@ -12,7 +11,7 @@ class User < ApplicationRecord
   end
 
   def self.authenticate(user_name, password)
-    user = User.find_by({user_name:})
+    user = User.find_by({ user_name: })
     user_password = Password.new(user.password)
     if user_password == password
       user
