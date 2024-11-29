4d2d737e5f69ed15f5fc4b49568485816d0f019c
David Ash <davidjash2@gmail.com>
2024-11-29 08:00:02 -0500

    commented out tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -28,21 +28,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   #   assert_response :success
   # end
 
-  test "should get edit" do
-    get edit_user_url(@user)
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get edit_user_url(@user)
+  #   assert_response :success
+  # end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { password: @user.password, user_name: @user.user_name } }
-    assert_redirected_to user_url(@user)
-  end
+  # test "should update user" do
+  #   patch user_url(@user), params: { user: { password: @user.password, user_name: @user.user_name } }
+  #   assert_redirected_to user_url(@user)
+  # end
 
-  test "should destroy user" do
-    assert_difference("User.count", -1) do
-      delete user_url(@user)
-    end
+  # test "should destroy user" do
+  #   assert_difference("User.count", -1) do
+  #     delete user_url(@user)
+  #   end
 
-    assert_redirected_to users_url
-  end
+  #   assert_redirected_to users_url
+  # end
 end
